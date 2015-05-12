1c668d7b74f2b714436a781de3056867434b624e
Aaron Larner <anlarner@gmail.com>
2015-05-12 11:59:52 -0500

    Add day 6

---
+++ day6/javascript-intro/main.js
@@ -0,0 +1,137 @@
+/*
+Step 1: console.log(10)
+Step 2: Show the value 10 to the console
+*/
+console.log(5+5);
+
+/*
+Step 1: console.log(10-3)
+Step 2: console.log(7)
+Step 3: Show the value 7 to the console
+*/
+console.log(5+5-3);
+
+console.log(5*3-1);
+
+/*
+Step 1: console.log(5*(2))
+Step 2: console.log(10)
+Step 3: Show the value 10 to the console
+*/
+console.log(5*(3-1));
+
+console.log('hello world');
+console.log('');
+
+console.log('My name is '+'Aaron');
+
+var myName = 'Bob Smith';
+
+
+/*
+Step 1: console.log('Hello, my name is '+'Aaron Larner')
+Step 2: Show the value 'Aaron Larner' to the console
+*/
+console.log('Hello, my name is '+myName);
+console.log('Hello, my name is '+myName);
+console.log('Hello, my name is '+myName);
+console.log('Hello, my name is '+myName);
+console.log('Hello, my name is '+myName);
+console.log('Hello, my name is '+myName);
+console.log('Hello, my name is '+myName);
+console.log('Hello, my name is '+myName);
+console.log('Hello, my name is '+myName);
+
+console.log(4+3+'7');
+
+console.log(myName.length);
+
+console.log(typeof '7');
+console.log(parseInt('7.7'));
+console.log(parseFloat('7.7'));
+
+console.log(6/3);
+console.log(-6/0);
+console.log('a'/4);
+
+console.log(Math.random())
+
+console.log(Math.floor(7.8));
+console.log(Math.ceil(7.8));
+
+console.log(myName.charAt(0));
+
+
+console.log(myName.substring(2, 3));
+
+
+console.log(true);
+console.log(false);
+console.log(typeof true);
+console.log(typeof (typeof true));
+
+
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+var shirtIsRed = false;
+var shoesAreBrown = true;
+
+console.log(shirtIsRed && shoesAreBrown);
+
+
+console.log(true && true && false);
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+console.log(true && false || true);
+console.log(false || true);
+
+console.log(5 > 6);
+console.log(5 > 5);
+console.log(5 >= 5);
+console.log(5 < 6);
+console.log(5 <= 6);
+
+console.log(5 === 5);
+console.log(5 === 6);
+console.log('5' === 5);
+console.log('5' == 5);
+
+console.log(!true);
+
+console.log(5 !== '5');
+
+var myAge = 3;
+
+if(myAge < 21) {
+	console.log("You can't view this website");
+}
+else {
+	console.log('You are old enough to view this website.')
+}
+
+if(myAge < 10) {
+	console.log('You are less than 10');
+}
+else if(myAge < 21) {
+	console.log('You are not old enough to drink');
+}
+else {
+	console.log('Let\'s party!');
+}
+
+console.log('A message from our sponsors');
+
+if(Math.random() < 0.5) {
+	console.log('Hello world');
+}
+else {
+	console.log('Foo bar');
+}
+
