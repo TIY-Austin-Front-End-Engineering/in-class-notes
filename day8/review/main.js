dca09eb7582d9614b93e895cd74f0843ab814f06
Aaron Larner <anlarner@gmail.com>
2015-05-14 10:38:52 -0500

    Day 8 review notes

---
+++ day8/review/main.js
@@ -0,0 +1,105 @@
+// var array = [1,2,3,4,5];
+
+
+// for (var i = 0; i < array.length; i++) {
+
+
+// 	console.log(++array[i])
+// // array[i]++;
+// // console.log(array[i]);
+	
+// }
+
+
+// var myArray = [];
+// var myOtherArray = ['Aaron', 'Larner'];
+
+// myArray.push(myOtherArray);
+
+// console.log(myArray);
+
+// var myString = 'hello,world,this,is,a test';
+
+// var myArray = [myString];
+// var myArray = [];
+// myArray.push(myString);
+
+// var myOtherArray = myString.split('');
+
+// console.log(myOtherArray);
+// console.log(myArray);
+
+// var myExampleArray = [
+// 	'foo',
+// 	'bar',
+// 	'baz',
+// 	'test'
+// ];
+
+// var myString = myExampleArray.join('*-*');
+
+// console.log(myString);
+
+// var myString = 'hello,world,this,is,a test';
+
+// var myArray = [myString];
+// var myArray = [];
+// myArray.push(myString);
+
+// var myOtherArray = myString.split('');
+
+// console.log(myOtherArray);
+// console.log(myArray);
+
+// var finished = false;
+// while(!finished) {
+// 	finished = true;
+// }
+
+
+// var keepGoing = true;
+// while(keepGoing) {
+// 	keepGoing = false;
+// }
+
+
+// var keepGoing = false;
+// do {
+// 	keepGoing = true;
+// } while(keepGoing);
+
+
+var myArray = [
+	3,
+	6,
+	9,
+	12,
+	15,
+	18,
+	21,
+	24,
+	27,
+	30
+];
+
+for(var i = 0; i < 10; i = i + 1) {
+	if(myArray[i]%2 === 0) {
+		console.log(myArray[i]);
+	}
+}
+
+// var arrayName = ['hello'];
+
+// arrayName[0] = 'world';
+
+// arrayName.push('foo');
+// arrayName.unshift('hello');
+// arrayName.pop()
+
+
+
+
+
+
+
+
