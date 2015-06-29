35bfffbec399d15dd91fc992ac8f52eb54cdc5dc
Aaron Larner <anlarner@gmail.com>
2015-06-29 12:37:34 -0500

    day 34

--- day-34/express-email/app.js
+++ day-34/express-email/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+// var messages = require('./routes/message');
 
 var app = express();
 
@@ -22,8 +23,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', routes);
 app.use('/users', users);
+// app.use('/messages', messages);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
